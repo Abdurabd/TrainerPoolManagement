a606bf2f263fbb25cb035bb6c2121ceccced7d32
Abdurabd <49178533+Abdurabd@users.noreply.github.com>
2019-09-18 15:27:53 +0530

    Add files via upload

---
+++ BAL/Business_Access_Layer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using BusinessObject;
+using System.Data;
+
+namespace BusinessAccessLayer
+{
+    public class Business_Access_Layer
+    {
+        DataAccessLayer.Data_Access_Layer sda1 = new DataAccessLayer.Data_Access_Layer();
+        public int AddBALRecord(BusinessObject.Business_Object ob)
+        {
+            return sda1.AddRecord(ob);
+        }
+        public DataTable LoginPage(BusinessObject.Business_Object ob)
+        {
+            return sda1.Loginpager(ob);
+        }
+        public DataSet Requestor()
+        {
+            return sda1.Request();
+        }
+    }
+}
